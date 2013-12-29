zhujll/android_device_xiaomi_aries-1

rename tree [PART 2]
Change-Id: Idecd7a0599e6b86cd4453099104db777a4d50127

BoardConfig.mk
@@ -36,12 +36,12 @@ TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/google/msm
-TARGET_KERNEL_CONFIG := cyanogen_mako_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_aries_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
@@ -54,11 +54,11 @@ BLUETOOTH_HCI_USE_MCT := true
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOOTLOADER_BOARD_NAME := MAKO
-TARGET_BOOTLOADER_NAME=mako
-TARGET_BOARD_INFO_FILE := device/lge/mako/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := ARIES
+TARGET_BOOTLOADER_NAME=aries
+TARGET_BOARD_INFO_FILE := device/xiaomi/aries/board-info.txt
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/mako/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/xiaomi/aries/bluetooth
 
 # Use the CM PowerHAL
 TARGET_USES_CM_POWERHAL := true
@@ -74,7 +74,7 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 
-BOARD_EGL_CFG := device/lge/mako/egl.cfg
+BOARD_EGL_CFG := device/xiaomi/aries/egl.cfg
 
 #BOARD_USES_HGL := true
 #BOARD_USES_OVERLAY := true
@@ -85,9 +85,9 @@ TARGET_USES_SF_BYPASS := true
 TARGET_USES_C2D_COMPOSITION := true
 
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_UI_LIB := librecovery_ui_mako
+TARGET_RECOVERY_UI_LIB := librecovery_ui_aries
 
-TARGET_RECOVERY_FSTAB = device/lge/mako/fstab.mako
+TARGET_RECOVERY_FSTAB = device/xiaomi/aries/fstab.aries
 RECOVERY_FSTAB_VERSION = 2
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 23068672 # 22M
@@ -102,13 +102,13 @@ BOARD_USES_SECURE_SERVICES := true
 BOARD_USES_EXTRA_THERMAL_SENSOR := true
 BOARD_USES_CAMERA_FAST_AUTOFOCUS := true
 
-BOARD_LIB_DUMPSTATE := libdumpstate.mako
+BOARD_LIB_DUMPSTATE := libdumpstate.aries
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 TARGET_NO_RPC := true
 TARGET_PROVIDES_GPS_LOC_API := true
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/lge/mako
+TARGET_RELEASETOOLS_EXTENSIONS := device/xiaomi/aries
 
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
@@ -122,12 +122,12 @@ TARGET_QCOM_DISPLAY_VARIANT := caf
 TARGET_QCOM_MEDIA_VARIANT := caf
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
 
--include vendor/lge/mako/BoardConfigVendor.mk
+-include vendor/xiaomi/aries/BoardConfigVendor.mk
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 BOARD_SEPOLICY_DIRS += \
-        device/lge/mako/sepolicy
+        device/xiaomi/aries/sepolicy
 
 BOARD_SEPOLICY_UNION += \
 	file_contexts \
@@ -159,4 +159,4 @@ BOARD_SEPOLICY_UNION += \
 	ueventd.te \
 	wpa_supplicant.te
 
-BOARD_HARDWARE_CLASS := device/lge/mako/cmhw/
+BOARD_HARDWARE_CLASS := device/xiaomi/aries/cmhw/

full_aries.mk
@@ -23,13 +23,13 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := full_mako
-PRODUCT_DEVICE := mako
+PRODUCT_NAME := full_aries
+PRODUCT_DEVICE := aries
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Nexus 4
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MODEL := MI 2
+PRODUCT_MANUFACTURER := XIAOMI
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/mako/device.mk)
-$(call inherit-product-if-exists, vendor/lge/mako/mako-vendor.mk)
+$(call inherit-product, device/xiaomi/aries/device.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/aries/aries-vendor.mk)

M1cha/backup_android_device_xiaomi_aries

Conflicts:
	BoardConfig.mk
	cm.mk
	device.mk
	extract-files.sh
	full_aries.mk
	init.aries.rc
	liblight/Android.mk
	liblight/lights.c
	sepolicy/bluetooth_loader.te
	sepolicy/file_contexts
	setup-makefiles.sh

BoardConfig.mk
@@ -34,14 +34,14 @@ TARGET_KRAIT_BIONIC_BBTHRESH := 64
 TARGET_KRAIT_BIONIC_PLDSIZE := 64
 
 TARGET_NO_BOOTLOADER := true
-TARGET_KERNEL_CONFIG := mako_defconfig
+TARGET_KERNEL_CONFIG := aries_defconfig
 
 # Use JWR66Y kernel compatibility fix
 TARGET_USE_JWR66Y := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 BOARD_USES_ALSA_AUDIO:= true
@@ -55,11 +55,11 @@ BLUETOOTH_HCI_USE_MCT := true
 
 TARGET_NO_RADIOIMAGE := true
 TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOOTLOADER_BOARD_NAME := MAKO
-TARGET_BOOTLOADER_NAME=mako
-TARGET_BOARD_INFO_FILE := device/lge/mako/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := ARIES
+TARGET_BOOTLOADER_NAME=aries
+TARGET_BOARD_INFO_FILE := device/xiaomi/aries/board-info.txt
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/mako/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/xiaomi/aries/bluetooth
 
 # FIXME: HOSTAPD-derived wifi driver
 BOARD_HAS_QCOM_WLAN := true
@@ -72,7 +72,7 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 
-BOARD_EGL_CFG := device/lge/mako/egl.cfg
+BOARD_EGL_CFG := device/xiaomi/aries/egl.cfg
 
 #BOARD_USES_HGL := true
 #BOARD_USES_OVERLAY := true
@@ -83,9 +83,9 @@ TARGET_USES_SF_BYPASS := true
 TARGET_USES_C2D_COMPOSITON := true
 
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_UI_LIB := librecovery_ui_mako
+TARGET_RECOVERY_UI_LIB := librecovery_ui_aries
 
-TARGET_RECOVERY_FSTAB = device/lge/mako/fstab.mako
+TARGET_RECOVERY_FSTAB = device/xiaomi/aries/fstab.aries
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 23068672 # 22M
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 23068672 # 22M
@@ -99,15 +99,15 @@ BOARD_USES_SECURE_SERVICES := true
 BOARD_USES_EXTRA_THERMAL_SENSOR := true
 BOARD_USES_CAMERA_FAST_AUTOFOCUS := true
 
-BOARD_LIB_DUMPSTATE := libdumpstate.mako
+BOARD_LIB_DUMPSTATE := libdumpstate.aries
 
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 TARGET_NO_RPC := true
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/lge/mako
+TARGET_RELEASETOOLS_EXTENSIONS := device/xiaomi/aries
 
 BOARD_SEPOLICY_DIRS := \
-       device/lge/mako/sepolicy
+       device/xiaomi/aries/sepolicy
 
 BOARD_SEPOLICY_UNION := \
        app.te \
@@ -135,4 +135,4 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 USE_DEVICE_SPECIFIC_CAMERA := true
 
--include vendor/lge/mako/BoardConfigVendor.mk
+-include vendor/xiaomi/aries/BoardConfigVendor.mk

full_aries.mk
@@ -23,13 +23,13 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := full_mako
-PRODUCT_DEVICE := mako
+PRODUCT_NAME := full_aries
+PRODUCT_DEVICE := aries
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Mako
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MODEL := MI 2
+PRODUCT_MANUFACTURER := XIAOMI
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/mako/device.mk)
-$(call inherit-product-if-exists, vendor/lge/mako/device-vendor.mk)
+$(call inherit-product, device/xiaomi/aries/device.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/aries/device-vendor.mk)